f358d99e57a1052b0be2b5e51e271a279dd417e9
Erkebulan-Akhmediya <yerkebulan20030921@gmail.com>
2025-08-08 17:03:52 +0500

    added password changing functionality

--- users/controller.go
+++ users/controller.go
@@ -1,14 +1,73 @@
 package users
 
 import (
+	"encoding/json"
+	"errors"
 	"io"
+	"log"
 	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
 )
+
+type updatePasswordDto struct {
+	OldPassword string `json:"oldPassword"`
+	NewPassword string `json:"newPassword"`
+}
 
 func updatePassword(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "PATCH" {
 		http.Error(w, "Request method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	io.WriteString(w, "Update password successfully")
+
+	var dto updatePasswordDto
+	if err := json.NewDecoder(r.Body).Decode(&dto); err != nil {
+		log.Println(err)
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	if dto.OldPassword == dto.NewPassword {
+		w.WriteHeader(http.StatusOK)
+		_, _ = io.WriteString(w, "Passwords are identical")
+		return
+	}
+
+	userId := r.Context().Value("userId").(int)
+	user, err := GetById(userId)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	if err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(dto.OldPassword)); err != nil {
+		log.Println(err)
+		http.Error(w, "Password incorrect", http.StatusBadRequest)
+		return
+	}
+
+	newPassword, err := bcrypt.GenerateFromPassword([]byte(dto.NewPassword), bcrypt.DefaultCost)
+	if errors.Is(err, bcrypt.ErrPasswordTooLong) {
+		http.Error(w, "Password too long", http.StatusBadRequest)
+		return
+	}
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	user.Password = string(newPassword)
+	if err = Update(user); err != nil {
+		log.Println(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	if _, err = io.WriteString(w, "Update password successfully"); err != nil {
+		log.Println(err)
+	}
 }
--- users/service.go
+++ users/service.go
@@ -10,10 +10,19 @@ type User struct {
 	Password string
 }
 
+func GetById(id int) (*User, error) {
+	query := `select id, username, password from "user" where id = $1`
+	return get(query, id)
+}
+
 func GetByUsername(username string) (*User, error) {
-	var user User
 	query := `select id, username, password from "user" where username = $1`
-	row := database.Db().QueryRow(query, username)
+	return get(query, username)
+}
+
+func get(query string, args ...any) (*User, error) {
+	var user User
+	row := database.Db().QueryRow(query, args...)
 	if err := row.Scan(&user.Id, &user.Username, &user.Password); err != nil {
 		return nil, err
 	}
@@ -23,5 +32,11 @@ func GetByUsername(username string) (*User, error) {
 func Create(username string, password string) error {
 	query := `INSERT INTO "user" (username, password) VALUES ($1, $2)`
 	_, err := database.Db().Exec(query, username, password)
+	return err
+}
+
+func Update(user *User) error {
+	query := `UPDATE "user" SET username = $1, password = $2 where id = $3`
+	_, err := database.Db().Exec(query, user.Username, user.Password, user.Id)
 	return err
 }
