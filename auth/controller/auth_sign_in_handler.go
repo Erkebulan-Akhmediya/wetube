bffcde9a2d958ef0554ef03ba67c7871538dccb6
Erkebulan-Akhmediya <yerkebulan20030921@gmail.com>
2025-09-06 19:57:10 +0500

    rewrote auth handlers

---
+++ auth/controller/auth_sign_in_handler.go
@@ -0,0 +1,57 @@
+package controller
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	authService "wetube/auth/service"
+	"wetube/users/service"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func NewSignInHandler() http.Handler {
+	return &signInHandler{}
+}
+
+type signInHandler struct{}
+
+func (sh *signInHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	dto, code, err := getAuthDto(r)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), code)
+		return
+	}
+	user, err := service.GetByUsername(dto.Username)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Username or password incorrect", http.StatusBadRequest)
+		return
+	}
+
+	if user.DeletedAt.Valid {
+		http.Error(w, "Your account has been deleted", http.StatusBadRequest)
+		return
+	}
+
+	if err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(dto.Password)); err != nil {
+		log.Println(err)
+		http.Error(w, "Username or password incorrect", http.StatusBadRequest)
+		return
+	}
+
+	token, err := authService.Create(user.Id, user.Password)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	if err = json.NewEncoder(w).Encode(jwtDto{Token: token}); err != nil {
+		log.Println(err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+}
---
+++ auth/controller/auth_sign_up_handler.go
@@ -0,0 +1,56 @@
+package controller
+
+import (
+	"database/sql"
+	"errors"
+	"log"
+	"net/http"
+	"wetube/users/service"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func NewSignUpHandler() http.Handler {
+	return &signUpHandler{}
+}
+
+type signUpHandler struct{}
+
+func (sh *signUpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	dto, code, err := getAuthDto(r)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), code)
+		return
+	}
+
+	user, err := service.GetByUsername(dto.Username)
+	if err != nil && !errors.Is(err, sql.ErrNoRows) {
+		log.Println(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	if user != nil {
+		http.Error(w, "User already exists", http.StatusBadRequest)
+		return
+	}
+
+	pwd, err := bcrypt.GenerateFromPassword([]byte(dto.Password), bcrypt.DefaultCost)
+	if errors.Is(err, bcrypt.ErrPasswordTooLong) {
+		http.Error(w, "Password is too long", http.StatusBadRequest)
+		return
+	}
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	if err = service.Create(dto.Username, string(pwd)); err != nil {
+		log.Println(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
