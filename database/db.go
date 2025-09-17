877c43909be4d3eba89dc747cd6ca68d7ea8a3fa
Erkebulan-Akhmediya <yerkebulan20030921@gmail.com>
2025-09-17 06:54:10 +0500

    hidden db credentials in env vars

--- database/db.go
+++ database/db.go
@@ -2,6 +2,8 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
+	"os"
 
 	_ "github.com/lib/pq"
 )
@@ -10,7 +12,13 @@ var db *sql.DB
 
 func Open() error {
 	var err error
-	connStr := "host=localhost port=2122 user=postgres password=123456 dbname=postgres sslmode=disable"
+	connStr := "host=%s port=%s user=%s password=%s dbname=%s sslmode=disable"
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	connStr = fmt.Sprintf(connStr, dbHost, dbPort, dbUser, dbPassword, dbName)
 	db, err = sql.Open("postgres", connStr)
 	if err != nil {
 		return err
